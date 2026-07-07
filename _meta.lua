555fa51439c322dc85ed5fbcd2243f90c09f5d4a
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-07 13:51:45 +0200

    chore: bump version to 1.0.1

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name        = "crossword",
     fullname    = _("Crossword"),
     description = _("Classic crossword puzzles. Fill the grid from the across and down clues."),
-    version     = "1.0.0",
+    version     = "1.0.1",
 }
